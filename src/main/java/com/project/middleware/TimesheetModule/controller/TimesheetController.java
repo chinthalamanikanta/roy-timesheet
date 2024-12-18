a691c7f701025129835cb19370fc97fcba66c636
Yugander <yugandhar@middlewaretalents.com>
2024-12-18 10:50:54 +0530

    Third Commit

--- src/main/java/com/project/middleware/TimesheetModule/controller/TimesheetController.java
+++ src/main/java/com/project/middleware/TimesheetModule/controller/TimesheetController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://talebts-frontend.azurewebsites.net")
 @RequestMapping("/api/timesheets")
 public class TimesheetController {
 
